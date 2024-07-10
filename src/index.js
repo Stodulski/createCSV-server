c350732d1c5942f150d81f7e1d0381e759816509
natasha <natasha87050@gmail.com>
2024-07-10 16:18:38 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const corsOptions = {
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
